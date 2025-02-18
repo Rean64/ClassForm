282b529375768df99bc15481bf3294ef745533ec
Rean64 <135015316+Rean64@users.noreply.github.com>
2025-02-18 12:58:44 +0100

    Update index.js
    empty

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-const fs = require('fs');
 
-const textIn = fs.readFileSync('./txt.txt', 'utf-8')
-
-const textOut = `This is: ${textIn}.\nCreated to leave everything today's ${Date.now()}`;
-fs.writeFileSync('./output.txt', textOut)
-
-console.log('File Written');
